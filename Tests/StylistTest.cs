7587cdf8ed1d313722c017a683e131294a17e891
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-09 08:56:24 -0700

    add new test for displaying list of current stylist employees

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -23,6 +23,18 @@ namespace HairSalon
     }
 
     [Fact]
+    public void Test_DisplayListOfStylist()
+    {
+      Stylist newStylist = new Stylist("Hilary");
+      newStylist.Save();
+
+      List<Stylist> ListOfStylist = Stylist.GetAll();
+      List<Stylist> DefaultList = new List<Stylist>{newStylist};
+
+      Assert.Equal(ListOfStylist, DefaultList);
+    }
+
+    [Fact]
     public void Test_AddNameToDatabase_Hilary()
     {
       Stylist newStylist = new Stylist("Hilary");
