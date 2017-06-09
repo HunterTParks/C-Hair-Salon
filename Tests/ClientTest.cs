954677fcd0ee8ade3d14a1bac07da9b558f678e4
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-09 09:01:15 -0700

    add initialization of a Client object testing file

---
+++ Tests/ClientTest.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  [Collection("HairSalon")]
+  public class ClientTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString  = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Client.DeleteAll();
+    }
+  }
+}
