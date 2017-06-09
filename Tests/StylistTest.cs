6510eb6c015fe25061d1092966557a23aa7a4142
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-09 08:17:08 -0700

    add initial test file for Stylist class; adds edit to namespace in Startup

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  [Collection("HairSalon")]
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString  = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+    
+  }
+}
